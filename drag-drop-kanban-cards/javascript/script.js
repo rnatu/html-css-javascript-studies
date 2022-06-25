d9086af48a4ad440df6daf4f9a597d46dcc06e5e
Renato <rnatu91@gmail.com>
2022-06-25 19:09:37 -0300

    feat: add class on the card is dragover and dragleave

---
+++ drag-drop-kanban-cards/javascript/script.js
@@ -0,0 +1,54 @@
+const cards = document.querySelectorAll(".card");
+const dropzones = document.querySelectorAll(".dropzone");
+
+// our cards
+cards.forEach((card) => {
+  card.addEventListener("drag", drag);
+  card.addEventListener("dragstart", dragstart);
+  card.addEventListener("dragend", dragend);
+});
+
+function dragstart({ target }) {
+  dropzones.forEach((dropzone) => {
+    dropzone.classList.add("highlight");
+  });
+
+  target.classList.add("is-dragging");
+}
+
+function drag() {
+  // console.log("drag");
+}
+
+function dragend({ target }) {
+  dropzones.forEach((dropzone) => {
+    dropzone.classList.remove("highlight");
+    target.classList.remove("is-dragging");
+  });
+
+  target.classList.remove("is-dragging");
+}
+
+// // our dropzone card
+dropzones.forEach((dropzone) => {
+  //   dropzone.addEventListener("dragenter", dragenter);
+  dropzone.addEventListener("dragleave", () => dragleave(dropzone));
+  dropzone.addEventListener("dragover", (event) => dragover(event, dropzone));
+  //   dropzone.addEventListener("drop", drop);
+});
+
+// function dragenter() {
+//   console.log("dragenter");
+// }
+function dragleave(dropzone) {
+  console.log(dropzone);
+  dropzone.classList.remove("over");
+}
+
+function dragover(event, dropzone) {
+  event.preventDefault();
+  dropzone.classList.add("over");
+}
+// function drop() {
+//   console.log("drop");
+// }
